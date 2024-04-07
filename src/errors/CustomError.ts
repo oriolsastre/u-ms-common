735ab6cb7d0582471c397f21fd4f7af56790ede1
Oriol Sastre <oriol.sastre@gmail.com>
2024-04-07 13:35:57 +0200

    Updates

---
+++ src/errors/CustomError.ts
@@ -0,0 +1,11 @@
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    // Because we are extending a built in class. Ho fem servir per comprovar el instanceof
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): { message: string; field?: string }[];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-interface Color {
-  red: number;
-  blue: number;
-  green: number;
-}
+// ERRORS
+export * from "./errors/BadRequestError";
+export * from "./errors/CustomError";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-found-error";
+export * from "./errors/NotAuthorizedError";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-  red: 20,
-  blue: 10,
-  green: 10,
-};
-
-console.log(color);
-
-export default color;
+// MIDDLEWARES
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validateRequest";
